51da795d368ee9658fbd4b042f63147cd91c61f0
dennis84 <d.dietrich84@gmail.com>
2014-11-15 12:22:00 +0100

    relative paths

--- slcollab.py
+++ slcollab.py
@@ -7,7 +7,17 @@ Co = Collab()
 
 def get_path(view):
     path = view.file_name()
-    return path if path != None else '[No Name]'
+    if path == None:
+        return '[No Name]'
+
+    path = os.path.normpath(view.file_name())
+    for project_path in view.window().folders():
+        project_path = os.path.normpath(project_path)
+        project_dir = os.path.basename(project_path)
+        if path.startswith(project_path):
+            path = path.replace(project_path, project_dir)
+
+    return path
 
 class CollabConnectCommand(sublime_plugin.WindowCommand):
     def run(self, local=False):
